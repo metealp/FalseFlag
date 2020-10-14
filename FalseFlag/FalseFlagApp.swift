4f1a4178138a707fb06862710a1924dd0f3a07a9
Mete Alp Kızılçay <kizilcaymetealp@gmail.com>
2020-10-14 23:38:02 +0300

    Initial Commit

---
+++ FalseFlag/FalseFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  FalseFlagApp.swift
+//  FalseFlag
+//
+//  Created by Mete Alp Kizilcay on 14.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct FalseFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
