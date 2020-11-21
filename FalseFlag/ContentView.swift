6b0ef366cbe9a9d586db233c95de5b9bcb20c008
Mete Alp Kızılçay <kizilcaymetealp@gmail.com>
2020-11-21 16:47:19 +0300

    fix accessibility hack

--- FalseFlag/ContentView.swift
+++ FalseFlag/ContentView.swift
@@ -8,15 +8,30 @@
 import SwiftUI
 
 
-var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
-var correctAnswer = Int.random(in: 0...2)
-
 struct ContentView: View {
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var showingScore = false
     @State private var scoreTitle = ""
     @State private var scoreMessage = ""
     @State private var score: Int = 0
-    @State private var tappedFlagIndex: Int = 0
+    @State private var animationAmount = 0.0
+
     var body: some View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
@@ -36,7 +51,13 @@ struct ContentView: View {
                     Button(action: {
                         self.flagTapped(number)
                     }){
-                        FlagImage(imageName: countries[number])
+//                        FlagImage(imageName: countries[number])
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
+                            
                     }
                     .padding()
                 }
@@ -57,17 +78,19 @@ struct ContentView: View {
             scoreTitle = "Correct"
             scoreMessage = "Well done! Play again?"
             score += 1
+            withAnimation {
+                self.animationAmount += 360
+            }
         } else {
-            tappedFlagIndex = number
             scoreTitle = "Wrong"
-            scoreMessage = "Wrong! That’s the flag of \(countries[tappedFlagIndex])"
+            scoreMessage = "Wrong! That’s the flag of \(countries[number])"
         }
         showingScore = true
     }
 
     func askQuestion() {
         countries.shuffle()
-        correctAnswer = Int.random(in: 0...2)
+        self.correctAnswer = Int.random(in: 0...2)
         showingScore = false
     }
 }
